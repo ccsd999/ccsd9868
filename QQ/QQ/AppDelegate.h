d046c7dd90677815049c58f9a0eef2b73da21865
Apple <Apple@cc.local>
2015-11-19 12:57:00 +0800

    创建了一个QQ项目

---
+++ QQ/QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by 陈冲 on 15/11/19.
+//  Copyright © 2015年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
